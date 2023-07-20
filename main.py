8657c63ea739cf04f72832d329102ef8a4341031
Bhuvan Noone <98878433+Shrek3147@users.noreply.github.com>
2023-07-20 21:05:56 +0530

    main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import pygame
 import sys
 from tkinter import filedialog
 from typing import List, Tuple, Optional
+from free_space import DiskSpaceGUI
 
 def open_directory():
     directory = filedialog.askdirectory()
@@ -15,12 +16,19 @@ def open_directory():
         file_tree = FileSystemTree(directory)
         visualize(file_tree)
 
+def check_free():
+    disk_space_gui = DiskSpaceGUI()
+    disk_space_gui.mainloop()
+
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("File System Visualizer")
+    root.title("File System Manager")
     root.geometry("400x100")
 
     visualize_button = tk.Button(root, text="Visualize", command=open_directory)
     visualize_button.pack(pady=10)
+
+    free_button = tk.Button(root,text="Show free space", command=check_free)
+    free_button.pack(pady=10)
 
     root.mainloop()
